4b3eaf3f57cc471c55696f6aa860b7892d63ba78
Nils Weidner <74982488+NilsWdnr@users.noreply.github.com>
2022-12-21 11:40:40 +0100

    Allowing to enter the login page, wasn't able if logged out of wordpress

--- HeadlessPress.php
+++ HeadlessPress.php
@@ -18,12 +18,16 @@ function remove_menus()
 
 add_action('admin_menu', 'remove_menus');
 
+function is_login_page() {
+    return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+}
+
 function remove_frontend()
 {
-    if (!is_admin() && !strpos($_SERVER['REQUEST_URI'], 'wp-json')) {
+    if (!is_admin() && ! is_login_page() && !strpos($_SERVER['REQUEST_URI'], 'wp-json')) {
         echo "Access denied";
         die;
     }
 }
 
-add_action('setup_theme', 'remove_frontend');
+add_action('after_setup_theme', 'remove_frontend');
